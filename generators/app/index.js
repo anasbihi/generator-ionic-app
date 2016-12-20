5350e95055fbaddcc2665b7d6f9ec47fb68c2485
Anas Bihi <anas.bihi@gmail.com>
2016-12-20 17:29:39 +0100

    setup generator architecture

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,19 +7,25 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the neat ' + chalk.red('generator-ionic-app') + ' generator!'
+      'Welcome to the ' + chalk.red('ionic-app') + ' generator!'
     ));
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+          type: 'confirm',
+          name: 'someAnswer',
+          message: 'Would you like to enable this option?',
+          store : true
+        },{
+          type: 'input',
+          name: 'name',
+          message : 'Your project name',
+          default : this.appname // Default to current folder name
+        }];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
+
     }.bind(this));
   },
 
